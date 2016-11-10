4e0350b50dd6d44153e3b4b18356c15e0ff86cd6
Aidan Fewster <fewstera@gmail.com>
2016-11-10 12:36:43 +0000

    Improvment: 🎉 no longer require jquery and pubsub to work

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-define(['bootstrap', 'istats-1'], function (news, istats) {
+define(['istats-1'], function (istats) {
+
+    var pageHasLoaded = false;
 
     var getQueryStringValue = function (queryString, param) {
         var reg = new RegExp('[?&]' + param + '=([^&#]*)', 'i'),
@@ -17,37 +19,61 @@ define(['bootstrap', 'istats-1'], function (news, istats) {
         }
     };
 
-    var hashOfThePage = window.location.hash.substr(1),
-        cpsAssetId = getQueryStringValue(hashOfThePage, 'cps_asset_id'),
-        pageType = getQueryStringValue(hashOfThePage, 'page_type'),
-        countername = getQueryStringValue(hashOfThePage, 'countername');
+    var trackPageLoad = function () {
+        var trackingContainer = document.createElement('p');
+        trackingContainer.style.position = 'absolute';
+        trackingContainer.style.top = '-999em';
+        trackingContainer.innerHTML = '<img src="' + window.istatsTrackingUrl + '" height="1" width="1" alt="" />';
 
-    window.istats = {
-        enabled: true
+        document.body.appendChild(trackingContainer);
     };
-    window.orb = {};
-    window.bbcFlagpoles_istats = 'ON';
-    window.istatsTrackingUrl = '//sa.bbc.co.uk/bbc/bbc/s?name=' + countername + '&cps_asset_id=' + cpsAssetId + '&page_type=' + pageType;
-    (function () {
-        if (window.location.href.split('onbbcdomain=')[1] === 'true') {
-            document.documentElement.className += ' onbbcdomain';
+
+    var setupIstats = function () {
+        var hashOfThePage = window.location.hash.substr(1),
+            cpsAssetId = getQueryStringValue(hashOfThePage, 'cps_asset_id'),
+            pageType = getQueryStringValue(hashOfThePage, 'page_type'),
+            countername = getQueryStringValue(hashOfThePage, 'countername');
+
+        window.istats = {
+            enabled: true
+        };
+        window.orb = {};
+        window.bbcFlagpoles_istats = 'ON';
+        window.istatsTrackingUrl = '//sa.bbc.co.uk/bbc/bbc/s?name=' + countername + '&cps_asset_id=' + cpsAssetId + '&page_type=' + pageType;
+    };
+
+    var setupPubsubListeners = function (callback) {
+        if (require.specified('bootstrap') || require.defined('bootstrap')) {
+            require(['bootstrap'], function (news) {
+                news.pubsub.on('istats', exports.istats);
+                news.pubsub.on('pageLoaded', exports.pageLoaded);
+                news.pubsub.on('app-share', exports.appShare);
+                callback();
+            });
+        } else {
+            callback();
         }
-        var hostId = window.location.href.match(/hostid=(.*)&/);
-        if (hostId && hostId.length) {
-            window.istatsTrackingUrl += '&iframe_host=' + encodeURI(hostId[1]);
+    };
+
+    var exports = {
+        init: function (callback) {
+            setupIstats();
+            setupPubsubListeners(callback);
+        },
+        istats: function (actionType, actionName, viewLabel) {
+            istats.log(actionType, actionName, {'view': viewLabel});
+        },
+        pageLoaded: function () {
+            if (!pageHasLoaded) {
+                hitEndpoint('bbcnewsapp://visualjournalism/pageloaded');
+                trackPageLoad();
+                pageHasLoaded = true;
+            }
+        },
+        appShare: function (appMessage) {
+            hitEndpoint('bbcnewsapp://visualjournalism/share?title=' + appMessage.title + '&text=' + appMessage.text, 'appShare');
         }
-    })();
-    document.body.innerHTML += '<' + 'p style="position: absolute; top: -999em;"><' + 'img src="' + window.istatsTrackingUrl + '" height="1" width="1" alt="" /><' + '/p>';
+    }
 
-    news.pubsub.on('istats', function (actionType, actionName, viewLabel) {
-        istats.log(actionType, actionName, {'view': viewLabel});
-    });
-
-    news.pubsub.on('pageLoaded', function () {
-        hitEndpoint('bbcnewsapp://visualjournalism/pageloaded');
-    });
-
-    news.pubsub.on('app-share', function (appMessage) {
-        hitEndpoint('bbcnewsapp://visualjournalism/share?title=' + appMessage.title + '&text=' + appMessage.text, 'appShare');
-    });
+    return exports;
 });
